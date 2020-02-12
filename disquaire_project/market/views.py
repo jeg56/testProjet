c920865251389784c1331e4a33a51d5acdf04a4b
Arnaud <arnaud.jegoux@gmail.com>
2020-02-13 00:19:11 +0100

    add map

--- disquaire_project/market/views.py
+++ disquaire_project/market/views.py
@@ -1,44 +1,95 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import refMarket
+from .models import refMarket,refPlageHoraire
 from django.db import connection
 from django.template import loader
+import requests
+from geopy.geocoders import Nominatim
 
 def index(request):
+   listMarket=refMarket.objects.all()
+   market=listMarket.filter()[:1].get()
+   if request.method == 'POST':
+      ville = request.POST.get('ville')
+      listMarket=refMarket.objects.filter(ville__icontains=ville)
+
+      if listMarket.exists():
+         market=listMarket[:1].get()
+     
+ 
+      
    context={
-      'listMarket':refMarket.objects.all()
+      'listMarket':listMarket,
+      'market':market
    }
    return render(request, 'market/index.html', context)
 
 
-
+_geolocator = Nominatim()   
 def upsert(request):
    messages=''
    title=''
+   showMap=''
+   latitude=None
+   longitude=None
+   latitude_fus=None
+   longitude_fus=None
+   nom=None
+   adresse=None
+   cp=None
+   ville=None
 
    if request.method == 'POST':
+      nom = request.POST.get('nom')
+      adresse = request.POST.get('adresse')
+      cp = request.POST.get('cp')
       ville = request.POST.get('ville')
-      action = request.POST.get('action')
+      plageHoraire= refPlageHoraire.objects.get(id=1)
       
 
-      verifVille = refMarket.objects.filter(ville=ville)
-      if verifVille.exists():
+      location = _geolocator.geocode(adresse+' '+cp +' '+ville, timeout=5)
+      if not location:
+         latitude=None
+         longitude=None
+      else:
+         latitude=location.latitude
+         longitude=location.longitude
+
+      verifNomMarche = refMarket.objects.filter(nom=nom)
+
+      if verifNomMarche.exists():
          # Attention le marché existe déja
          messages='Sauf erreur de ma part ce marché existe déjà...'
          title= "ECHEC !!!!"
       else:
          ajoutVille = refMarket.objects.create(
-            ville=ville
+            nom=nom,
+            adresse=adresse,
+            cp=cp,
+            ville=ville,
+            plageHoraire=plageHoraire,
+            latitude=latitude,
+            longitude=longitude
             )
+
+            
          ajoutVille.save()
          messages='Ok c est bon !!...'
          title='SUCCESSSS !!!!'
+         showMap='1'
+         longitude_fus=str(longitude).replace(',','.')
+         latitude_fus=str(latitude).replace(',','.')
    else:
       title= "Déclaration d'une nouvelle ville"
       
    context={
       'title':title,
-      'messages':messages
+      'messages':messages,
+      'showMap':showMap,
+      'latitude':latitude_fus,
+      'longitude':longitude_fus,
+      'adresse':'%s %s %s' % (adresse,cp,ville),
+      'nom':nom
    }
 
    return render(request, 'market/upsert.html', context)
